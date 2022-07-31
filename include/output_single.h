e83e2d8b2182214971a2e14a2908803c1c0fc800
Rainer Spurzem <spurzem@ari.uni-heidelberg.de>
2022-07-31 16:45:06 +0200

    include/output_binary.h and output_single.h need spin vector for HDF5

--- include/output_single.h
+++ include/output_single.h
@@ -7,7 +7,7 @@
      &                  S_D21(NMAX), S_D22(NMAX), S_D23(NMAX),
      &                  S_D31(NMAX), S_D32(NMAX), S_D33(NMAX),
      &            S_STEP(NMAX), S_STEPR(NMAX), S_T0(NMAX), S_T0R(NMAX),
-     &                  S_RS(NMAX), S_L(NMAX), S_TE(NMAX),
+     &                  S_ASPN(NMAX), S_RS(NMAX), S_L(NMAX), S_TE(NMAX),
      &                  S_RC(NMAX), S_MC(NMAX), S_POT(NMAX),
      &                  NS_KW(NMAX),NS_NAM(NMAX),NS_LAB(NMAX)
       REAL*4 S_AS, S_M, S_RSN, S_X1, S_X2, S_X3, S_V1, S_V2, S_V3
@@ -15,7 +15,7 @@
       REAL*4 S_FD1, S_FD2, S_FD3
       REAL*4 S_D21, S_D22, S_D23
       REAL*4 S_D31, S_D32, S_D33
-      REAL*4 S_STEP, S_STEPR, S_T0, S_T0R
+      REAL*4 S_STEP, S_STEPR, S_T0, S_T0R, S_ASPN
       REAL*4 S_RS, S_L, S_TE, S_RC, S_MC
       REAL*4 S_POT
       INTEGER NS_KW,NS_NAM,NS_LAB
